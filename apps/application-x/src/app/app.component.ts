cec8c530d125453aa6d6ea1d9acb51cbbcdb9e0f
suryateja-kandukuru <70886018+suryateja-kandukuru@users.noreply.github.com>
2023-01-02 23:11:48 +0530

    Initial commit

---
+++ apps/application-x/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'back-base-assignment-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'application-x';
+}
